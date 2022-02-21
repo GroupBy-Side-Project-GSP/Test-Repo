2afdb85b8bc5cd0c92af87d8ce12b130c73e5287
saehyun <sh00710000@gmail.com>
2022-02-21 21:00:54 +0900

    ⚡️ :: toast

--- app/src/main/java/com/gsp/test/MainActivity.kt
+++ app/src/main/java/com/gsp/test/MainActivity.kt
@@ -2,10 +2,13 @@ package com.gsp.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext, "test!", Toast.LENGTH_SHORT).show()
     }
 }
